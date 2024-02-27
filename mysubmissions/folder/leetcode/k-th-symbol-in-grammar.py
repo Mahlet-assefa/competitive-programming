bdb02d2f8768be91b5296bbe1c4fef16afc2d085
Mahlet Assefa <mahletassefa07@gmail.com>
2024-02-27 21:33:53 +0300

    Add solution for K-th Symbol in Grammar

---
+++ mysubmissions/folder/leetcode/k-th-symbol-in-grammar.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n==1:
+            return 0
+        else:
+            if k%2==0:
+                return 1-self.kthGrammar(n-1, ceil(k/2))
+            else:
+                return self.kthGrammar(n-1, ceil(k/2))
+        
